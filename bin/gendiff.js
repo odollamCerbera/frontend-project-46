458f92b2f225b663de743f30b4ff50170105c86a
Cerbera <bykovaem.klp@gmail.com>
2024-09-30 20:48:22 +0500

    Вывод справки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .argument('<filepath1>', 'First file address')
+  .argument('<filepath2>', 'Second file address');
+
+program.parse();
+
