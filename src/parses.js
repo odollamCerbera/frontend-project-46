b711ac1b918f4f54b775f59445cd6fcc68730b81
Cerbera <bykovaem.klp@gmail.com>
2024-10-20 21:33:00 +0500

    refactoring

--- src/parses.js
+++ src/parses.js
@@ -4,9 +4,9 @@ const parseFile = (filePath, format) => {
   switch (format) {
     case '.json':
       return JSON.parse(filePath);
-    case '.yml':
-      return yaml.load(filePath);
     case '.yaml':
+      return yaml.load(filePath);
+    case '.yml':
       return yaml.load(filePath);
     default:
       throw new Error(`Incorrect format: '${format}'`);
